2ff8eb2dcb8ba680855493869b17b31185327d43
Weal Ali <wealali595@gmail.com>
2020-01-16 13:38:23 +0100

    new change

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,5 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("new class in the name of Test for jenkins");
+        System.out.println("new class in the name of Test for jenkins project testing OOOOOOOOOOO");
     }
 }
